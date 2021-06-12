777d0ca0432b8cbdc0d62e4e7922c93a56257ebd
Leam Hall <leamhall@gmail.com>
2021-06-11 20:05:27 -0400

    Basics for unit tests.

--- lib/mins.php
+++ lib/mins.php
@@ -1,7 +1,8 @@
 <?php
   // Class minimum stats.
+  // Arrange highest required to lowest. Down the road we'll get rid of prefs.php and use this array's keys.
   $mins = array(
-    "Paladin"   => array("Charisma" => 17, "Wisdom" => 12, "Strength" => 12),
-    "Cleric"    => array("Wisdom" => 9),
+    "Paladin"   => array("Charisma" => 17, "Wisdom" => 13, "Strength" => 12, "Intelligence" => 9, "Constitution" => 9, "Dexterity" => 6),
+    "Cleric"    => array("Wisdom" => 9, "Strength" => 6, "Intelligence" => 6, "Charisma" => 6, "Constitution" => 6, "Dexterity" => 3),
   );
 ?>
--- tests/CharacterTest.php
+++ tests/CharacterTest.php
@@ -1,20 +1,85 @@
 <?php declare(strict_types=1);
 
+require dirname(__DIR__) . '/lib/character.php';
+require dirname(__DIR__) . '/lib/prefs.php';
+require dirname(__DIR__) . '/lib/mins.php';
+
 use PHPUnit\Framework\TestCase;
 
 final class CharacterTest extends TestCase
 {
   public function testCanBeCreated(): void
   {
-    $this->assertInstancedOf(
-      Character);
+    require dirname(__DIR__) . '/lib/prefs.php';
+    require dirname(__DIR__) . '/lib/mins.php';
+    $class      = "Paladin";
+    $mode       = 'short';  // $mode can currently be 'full' or 'short'
+    $name       = "Sally";
+    $race       = 'human';
+    $gender     = 'female';
+    $character  = new Character($class, $prefs, $mins, $race, $gender, $name);
+
+    $this->assertInstanceOf(Character::class, $character);
   }
+
+
+  public function testSetAttributes(): void
+  {
+    require dirname(__DIR__) . '/lib/prefs.php';
+    require dirname(__DIR__) . '/lib/mins.php';
+    $class      = "Paladin";
+    $mode       = 'short';  // $mode can currently be 'full' or 'short'
+    $name       = "Sally";
+    $race       = 'human';
+    $gender     = 'female';
+    $character  = new Character($class, $prefs, $mins, $race, $gender, $name);
+
+    $this->assertEquals('Sally',    $character->name);
+    $this->assertEquals('Paladin',  $character->class);
+    $this->assertEquals('human',    $character->race);
+    $this->assertEquals('female',   $character->gender);
+  }
+
+  public function testSetPaladinStats(): void
+  {
+    require dirname(__DIR__) . '/lib/prefs.php';
+    require dirname(__DIR__) . '/lib/mins.php';
+    $class      = "Paladin";
+    $mode       = 'short';  // $mode can currently be 'full' or 'short'
+    $name       = "Sally";
+    $race       = 'human';
+    $gender     = 'female';
+    $character  = new Character($class, $prefs, $mins, $race, $gender, $name);
+
+    $this->assertGreaterThan(11,  $character->stats['Strength']);
+    $this->assertGreaterThan(8,   $character->stats['Intelligence']);
+    $this->assertGreaterThan(12,  $character->stats['Wisdom']);
+    $this->assertGreaterThan(5,   $character->stats['Dexterity']);
+    $this->assertGreaterThan(8,   $character->stats['Constitution']);
+    $this->assertGreaterThan(16,  $character->stats['Charisma']);
+  }
+
+
+  public function testSetClericStats(): void
+  {
+    require dirname(__DIR__) . '/lib/prefs.php';
+    require dirname(__DIR__) . '/lib/mins.php';
+    $class      = "Cleric";
+    $mode       = 'short';  // $mode can currently be 'full' or 'short'
+    $name       = "Sally";
+    $race       = 'human';
+    $gender     = 'female';
+    $character  = new Character($class, $prefs, $mins, $race, $gender, $name);
+
+    $this->assertGreaterThan(5,  $character->stats['Strength']);
+    $this->assertGreaterThan(5,  $character->stats['Intelligence']);
+    $this->assertGreaterThan(8,  $character->stats['Wisdom']);
+    $this->assertGreaterThan(2,  $character->stats['Dexterity']);
+    $this->assertGreaterThan(5,  $character->stats['Constitution']);
+    $this->assertGreaterThan(5,  $character->stats['Charisma']);
+  }
+
 }
-
-
-
-
-
 
 ?>
 
